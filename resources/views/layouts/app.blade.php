b906b022d27a6f515da4dead958e635ffafdcc6d
Alan Arregui <alanarregui@gmail.com>
2025-04-15 08:48:23 -0300

    diseño front

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,8 @@
+@props(['title' => '', 'footerLinks' => ''])
+<x-base-layout title="">
+    <x-layouts.header />
+    {{ $slot }}
+    <footer>
+        {{-- @yield('footerLinks') --}}
+    </footer>
+</x-base-layout>
